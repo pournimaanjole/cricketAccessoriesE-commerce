fad2d60c1c360a4ecbaa66e6dcf8547205a112ba
pournimaanjole <pournimaanjole@gmail.com>
2023-12-27 10:23:32 +0530

    footer is creating

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div>
+      <div className='footer-container '>
+        <div className='footer-up '>
+<div className='footer-div'>
+    <img src={`https://thegodofsports.com/wp-content/uploads/2022/10/smartphone-1.png`}/>
+    <div>
+    <span>NO SHIPPING CHARGES</span>
+    <span>Get free shipping on all orders</span>
+    </div>
+   
+</div>
+<div className='footer-div'>
+    <img src={`https://thegodofsports.com/wp-content/uploads/2022/10/back-1.png`}/>
+    <div>
+    <span>CUSTOMER SERVICE</span>
+    <span>
+
+We are here to serve you</span>
+    </div>
+</div>
+<div className='footer-div'>
+    <img src={`https://thegodofsports.com/wp-content/uploads/2022/10/express-delivery-1.png`}/>
+    <div>
+    <span>100% AUTHENTIC PRODUCTS</span>
+    <span>
+
+All our products are 100% original</span>
+    </div>
+    
+</div>
+<div className='footer-div'>
+    <img src={`https://thegodofsports.com/wp-content/uploads/2022/10/padlock-1.png`}/>
+    <div>
+    <span>SECURE SHIPPING</span>
+    <span>
+
+Do not worry. Your in safe hands.</span>
+    </div>
+</div>
+        </div>
+        <div className='footer-down'>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -6,18 +6,33 @@ import HomeCarosoul from '../../components/HomeCarosoul/HomeCarosoul.js'
 import Navbar1 from '../../components/Navbar1/Navbar1.js'
 import Card from '../../components/Card/Card.js'
 import DiscountCard from '../../components/DiscountCard/DiscountCard.js'
+import badmintondata from './../Homeutils/badminton.json'
+import cricketdata from './../Homeutils/cricket.json'
+import footballdata from './../Homeutils/football.json'
+import tennisdata from './../Homeutils/tennis.json'
+import oppsimg from './opps.png'
 import shoes1 from './shoes.avif'
 import shoes2 from './shoes2.avif'
+import Footer from '../../components/Footer/Footer.js'
 import bat from './bat.jpeg'
 import badminto from './badminton.jpeg'
 const Home = () => {
-  const [choise,setChoise] = useState(Choise)
+  const [choise, setChoise] = useState(Choise)
+  const [badminton, setbadminton] = useState(badmintondata)
+  const [cricket, setcricket] = useState(cricketdata)
+  const [tennis, settennis] = useState(tennisdata)
+  const [football, setfootball] = useState(footballdata)
+  const [active, setActive] = useState(1)
+
+  const toactive = (index) => {
+    setActive(index)
+  }
   return (
     <div>
-      <Navbar1/>
-     <Navbar/>
-    <HomeCarosoul/>
-    {/* <div className='display'>
+      <Navbar1 />
+      <Navbar />
+      <HomeCarosoul />
+      {/* <div className='display'>
       {
         choise.map((data,index)=>{
           const {off,newprice,oldprice,brand,productName,ProductImg} = data
@@ -27,16 +42,115 @@ const Home = () => {
         })
       }
     </div> */}
-    <div className='discount-display'>
-    <DiscountCard img={shoes1} />
-    <DiscountCard img={shoes2} />
-    
-    <DiscountCard img={bat} />
-    <DiscountCard img={badminto} />
+      <div className='discount-display'>
+        <DiscountCard img={shoes1} />
+        <DiscountCard img={shoes2} />
+
+        <DiscountCard img={bat} />
+        <DiscountCard img={badminto} />
+      </div>
+<div className='display-head'>
+  <div ><span className='our-product'>Our Products</span></div>
+  <div className='display-heading'>
+
+    <div onClick={()=>{
+      toactive(1)
+    }}>
+<span>Badminton</span>
     </div>
-   
 
+    <div onClick={()=>{
+      toactive(2)
+    }}>
+<span>Cricket</span>
+    </div>
 
+    <div onClick={()=>{
+      toactive(3)
+    }}>
+<span>Tennis</span>
+    </div>
+
+    <div onClick={()=>{
+      toactive(4)
+    }}>
+<span>Football</span>
+
+    </div>
+    <div onClick={()=>{
+      toactive(5)
+    }}>
+      <span>Volleyball</span>
+    </div>
+
+  </div>
+</div>
+
+      <div>
+        <div >
+
+        
+        <div className={active === 1? "display-div contain-active" : " contain"}>
+          <div className='display-div'>
+          {
+            badminton.map((data, index) => {
+              const { off, newprice, oldprice, brand, productName, ProductImg } = data
+
+              return (<> <Card off={off} newprice={newprice} oldprice={oldprice} brand={brand} productName={productName} ProductImg={ProductImg} /> </>)
+            })
+          }
+          </div>
+         
+        </div> 
+        </div>
+        <div className={active === 2? "display-div contain-active" : "contain"}>
+          <div className='display-div'>
+          {
+            cricket.map((data, index) => {
+              const { off, newprice, oldprice, brand, productName, ProductImg } = data
+
+              return (<> <Card off={off} newprice={newprice} oldprice={oldprice} brand={brand} productName={productName} ProductImg={ProductImg} /> </>)
+            })
+          }
+          </div>
+         
+        </div>
+        <div className={active === 3? "display-div contain-active" : "contain"}>
+          <div className='display-div'>
+          {
+            football.map((data, index) => {
+              const { off, newprice, oldprice, brand, productName, ProductImg } = data
+
+              return (<> <Card off={off} newprice={newprice} oldprice={oldprice} brand={brand} productName={productName} ProductImg={ProductImg} /> </>)
+            })
+          }
+          </div>
+         
+        </div>
+        <div className={active === 4? "display-div contain-active" : "contain"}>
+          <div className='display-div'>
+
+          {
+            tennis.map((data, index) => {
+              const { off, newprice, oldprice, brand, productName, ProductImg } = data
+
+              return (<> <Card off={off} newprice={newprice} oldprice={oldprice} brand={brand} productName={productName} ProductImg={ProductImg} /> </>)
+            })
+          }
+          </div>
+          
+        </div> 
+
+        <div className={active === 5? "display-div contain-active" : "contain"}>
+          <div className='display-div'>
+<img src={oppsimg} className='opps-img'/>
+
+<div><span className='data'>Data is </span><span className='not-fount'>Not Found </span></div>
+          </div>
+        </div>
+
+      </div>
+      <Footer/>
     </div>
   )
 }
